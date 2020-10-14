5856fe114b8f3cf64742d99df626dbb2c2a8790c
Unknown <omegaclassic.official@gmail.com>
2020-10-14 14:30:44 +0300

    Changes

--- src/main/java/hwid/server/HWIDServer.java
+++ src/main/java/hwid/server/HWIDServer.java
@@ -5,22 +5,17 @@ import com.google.gson.GsonBuilder;
 import cpw.mods.fml.common.FMLLog;
 import cpw.mods.fml.common.event.FMLServerStartingEvent;
 import cpw.mods.fml.common.event.FMLServerStoppingEvent;
-import it.unimi.dsi.fastutil.ints.Int2ObjectMap;
-import it.unimi.dsi.fastutil.ints.Int2ObjectOpenHashMap;
 import org.apache.commons.io.FileUtils;
 
 import java.io.File;
 import java.io.IOException;
-import java.nio.file.Files;
 
 public class HWIDServer {
     private static Config config = new Config();
-    private static final File banned = new File("banned hwids.cfg");
-
+    private static final File banned = new File("banned hw.cfg");\
 
     public static void start(FMLServerStartingEvent event) {
         try {
-
             config = new Gson().fromJson(FileUtils.readFileToString(banned), Config.class);
         } catch (IOException e) {
             FMLLog.bigWarning("Cannot read banned hwids");
---
+++ src/main/java/hwid/server/command/CommandGuard.java
@@ -0,0 +1,22 @@
+package hwid.server.command;
+
+import net.minecraft.command.CommandBase;
+import net.minecraft.command.ICommandSender;
+
+public class CommandGuard extends CommandBase {
+
+    @Override
+    public String getCommandName() {
+        return "guard";
+    }
+
+    @Override
+    public String getCommandUsage(ICommandSender p_71518_1_) {
+        return "guard";
+    }
+
+    @Override
+    public void processCommand(ICommandSender p_71515_1_, String[] p_71515_2_) {
+
+    }
+}
