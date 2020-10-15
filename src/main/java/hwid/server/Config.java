9104069cf0d3ff6d8d4c52942feca4b56665c6c0
Unknown <omegaclassic.official@gmail.com>
2020-10-15 16:25:46 +0300

    Changes

--- src/main/java/hwid/server/Config.java
+++ src/main/java/hwid/server/Config.java
@@ -1,12 +1,15 @@
 package hwid.server;
 
+import com.mojang.realmsclient.util.Pair;
 import it.unimi.dsi.fastutil.ints.Int2ObjectMap;
 import it.unimi.dsi.fastutil.ints.Int2ObjectOpenHashMap;
 import it.unimi.dsi.fastutil.objects.Object2ObjectMap;
 import it.unimi.dsi.fastutil.objects.Object2ObjectOpenHashMap;
+import org.apache.commons.lang3.tuple.ImmutablePair;
+
+import java.util.Map;
 
 public class Config {
-    public Int2ObjectMap<String> hwidByHash = new Int2ObjectOpenHashMap<>(128);
-    public Object2ObjectMap<String, String> guard = new Object2ObjectOpenHashMap<>(64);
+    public Object2ObjectMap<String, ImmutablePair<String, String>> guard = new Object2ObjectOpenHashMap<>(64);
     public Object2ObjectMap<String, String> lastKnown = new Object2ObjectOpenHashMap<>(64);
 }
